e72ece1a0c4ce0e4b08a4c1a2f051b4050935ea2
Ramiro Gomez (RamequeJeque) <rgomez@attachdesign.com>
2011-11-27 19:04:20 -0500

    Optimization for create multiple galleries at same time
    Modify the plugin to create multiple galleries in different selectors,
    creating additional params for this.

--- js/jquery.attach.flickr.js
+++ js/jquery.attach.flickr.js
@@ -1,9 +1,10 @@
 (function( $ ) {
 	
 	
-	$.attachFlickr = {};
 	
-	$.attachFlickr.settings = {
+	$.flickrClass = function(){};
+	
+	$.flickrClass.prototype.settings = {
 		per_page:10,						//Photos per page
 		user_id:'',							//Any user id from flickr
 		tags:'',							//If you want to serach photos by tags
@@ -13,26 +14,31 @@
 		description:false,					//if you want a image description from flickr
 		clearContainer:false,				//If you want remove previous content in container
 		template:'interspersed', 			//interspersed , continuous, first-description, first-images,
-		tpl:null,							//template for locate images and description
-		repeattpl:'<span>{images}</span>', 	//repeat template for list images
+		tpl:'',							//template for locate images and description
+		repeattpl:'', 	//repeat template for list images
 		desCotentCSS:'content-css',			//css for content description span
 		descCSS:'des-css',					//css for description span
 		dateCSS:'date-css',					//css for date span
 		tagCSS:'label notice',				//css for tags
+		linkTagCSS:	'link-css',				//css for link for images			
 		titleImg:'title',					//if you want to change title attribute for anoither attribute (this is because i need this for my personal project)
 		callback_end:function(){}			//callback function for 
 	};
 	
-	$.attachFlickr.APIFlickr = {
+	
+	
+	$.flickr = {}
+	
+	$.flickr.counter = 0;
+	
+	$.flickr.APIFlickr = {
 		'flickr.photos.search': {api_key:"",user_id:"",tags:"",tag_mode:"",text:"",min_upload_date:"",max_upload_date:"",min_taken_date:"",max_taken_date:"",license:"",sort:"",privacy_filter:"",bbox:"",accuracy:"",safe_search:"",content_type:"",machine_tags:"",machine_tag_mode:"",group_id:"",contacts:"",woe_id:"",place_id:"",media:"",has_geo:"",geo_context:"",lat:"",lon:"",radius:"",radius_units:"",is_commons:"",in_gallery:"",is_getty:"",extras:"",per_page:"",page:""}
 	}
 	
-	$.attachFlickr.methods = {
+	$.flickr.methods = {
 		init : function( options ) { 
 		  // THIS 
-		  $.attachFlickr.settings._self = this;
-		  
-		  $.extend($.attachFlickr.settings, options);
+		  $.extend($.flickr.settings, options);
 		  
 			return this.each(function(){
 			  //console.log('init each');			  
@@ -42,78 +48,78 @@
 		show : function( ) {
 		  // IS
 		  //console.log('show');	
-		   $.extend($.attachFlickr.settings, options);
+		   $.extend($.flickr.settings, options);
 		},
 		hide : function( ) { 
 		  // GOOD
 		  //console.log('hide');
-		   $.extend($.attachFlickr.settings, options);
+		   $.extend($.flickr.settings, options);
 		},
 		update : function( content ) { 
 		  // !!! 
 		  //console.log('update');
-		   $.extend($.attachFlickr.settings, options);
+		   $.extend($.flickr.settings, options);
 		},
 		complete_panel: function(options){
-			//console.log('complete_panel');
-			$.extend($.attachFlickr.settings, options);			
-			$.attachFlickr.methods.__template($.attachFlickr.settings);
+			
+			$.extend($.flickr.settings, options);			
+			$.flickr.methods.__template(options);
 	
-			$.attachFlickr.settings.callback_end();
+			options.callback_end();
 		},
 		__template:function(options){
-			$.extend($.attachFlickr.settings, options);
-			var tpl = $.attachFlickr.settings.template;
+			$.extend($.flickr.settings, options);
+			var tpl = options.template;
 			
 			//console.log(tpl);
 			
 			switch(tpl) {
 				case 'interspersed':
-					$($.attachFlickr.settings.photos).each(function(item,value){
-						var descItem = $.attachFlickr.settings.images_description[item];
-						$($.attachFlickr.settings._self).append(value).append(descItem);
+					$($.flickr.settings.photos).each(function(item,value){
+						var descItem = $.flickr.settings.images_description[item];
+						$($.flickr.settings._self).append(value).append(descItem);
 					});
 					break
 					
 				case 'first-description':
-					$($.attachFlickr.settings.images_description).each(function(item,value){
-						$($.attachFlickr.settings._self).append(value);
+					$($.flickr.settings.images_description).each(function(item,value){
+						$($.flickr.settings._self).append(value);
 					});
-					$($.attachFlickr.settings.photos).each(function(item,value){
-						$($.attachFlickr.settings._self).append(value);
+					$($.flickr.settings.photos).each(function(item,value){
+						$($.flickr.settings._self).append(value);
 					});
 					break
 					
 				case 'first-images' :
 					
-					var newSelf = $($.attachFlickr.settings._self);
-					var newSelfDesc = $($.attachFlickr.settings._self);
+					var newSelf = $(options._self);
+					var newSelfDesc = $(options._self);
 					
-					if($.attachFlickr.settings.tpl){
-						$($.attachFlickr.settings._self).append($.attachFlickr.settings.tpl);
+					if(options.tpl){
+						$(options._self).append(options.tpl);
 						
-						var objSelf = $($.attachFlickr.settings._self).parent().find('*:contains("{images}"):last');
+						var objSelf = $(options._self).parent().find('*:contains("{images}"):last');
 						newSelf = objSelf;
 					}	
 					
-					$($.attachFlickr.settings.photos).each(function(item,value){
+					$(options.photos).each(function(item,value){
 						$(newSelf).append(value);
 					});
 					
 					
-					if($.attachFlickr.settings.tpl){
-						var objSelfDesc = $($.attachFlickr.settings._self).parent().find('*:contains("{description}"):last');
+					if(options.tpl){
+						var objSelfDesc = $(options._self).parent().find('*:contains("{description}"):last');
 						newSelfDesc = objSelfDesc;
 					}
 					
-					$($.attachFlickr.settings.images_description).each(function(item,value){
+					$(options.images_description).each(function(item,value){
 						$(newSelfDesc).append(value);
 					});
 					
 					
-					if($.attachFlickr.settings.tpl){
-						var images_a = $($.attachFlickr.settings._self).parent().find('*:contains("{images}")')
-						var description_a =$($.attachFlickr.settings._self).parent().find('*:contains("{description}")')
+					if(options.tpl){
+						var images_a = $(options._self).parent().find('*:contains("{images}")')
+						var description_a =$(options._self).parent().find('*:contains("{description}")')
 						
 						$.merge( images_a, description_a ).each(function(){
 								var __initHtml = $(this).html();
@@ -129,8 +135,8 @@
 				case 'continuous' :
 				case 'default' :
 				
-					$($.attachFlickr.settings.photos).each(function(item,value){
-						$($.attachFlickr.settings._self).append(value);
+					$(options.photos).each(function(item,value){
+						$(options._self).append(value);
 					});
 					break
 				
@@ -138,60 +144,67 @@
 		}
 		,
 		error: function(){
-			$($.attachFlickr.settings._self).append('Error de carga de datos: Revise su conexion de internet o su servicio de Flickr');	
+			$(options._self).append('Error de carga de datos: Revise su conexion de internet o su servicio de Flickr');	
 		}
 		,
 		photosSearch: function(options){
 			//
-			$.extend($.attachFlickr.settings, options);
+			
+
+			
 			//console.log('photosSearch');
 			//console.log(this.selector);
 			
 			return this.each(function(){
 			  //console.log('each');
-			  	$.attachFlickr.settings._self = this;
-				$.attachFlickr.methods._photos('flickr.photos.search',options);
+			  
+			  
+			  
+			  	var op = new $.flickrClass();
+				$.flickr.counter++;
+				
+				var mySettings = new Object();
+				$.extend(mySettings, op.settings);
+				
+				$.extend(mySettings, options);
+	
+			  	mySettings._self = this;
+				op.settings = mySettings;
+				$.flickr.methods._photos('flickr.photos.search',op.settings);
 			});
-			
-			
 		},
 		photosInfo: function(options){
 			//
-			$.extend($.attachFlickr.settings, options);
+			$.extend(options, options);
 			//console.log('photosSearch');
 			//console.log(this.selector);
 			
 			return this.each(function(){
 			  //console.log('photosInfo each');
-			  	$.attachFlickr.settings._self = this;
-				$.attachFlickr.methods.__clearContainer();
+			  	options._self = this;
+				$.flickr.methods.__clearContainer(options);
 				
-				$.attachFlickr.methods._photos('flickr.photos.getInfo',options);
+				$.flickr.methods._photos('flickr.photos.getInfo',options);
 			});
 			
 			
 		},
-		__createObjectMethod:function(method){
-			
+		__createObjectMethod:function(method,options){
 			var optionsMethod = {};
-			
-			var obj = $.attachFlickr.APIFlickr[method];
+			var obj = $.flickr.APIFlickr[method];
 				for(var prop in obj){
-					if($.attachFlickr.settings[prop] != undefined){
-						optionsMethod[prop] = $.attachFlickr.settings[prop];
+					if(options[prop] != undefined){
+						optionsMethod[prop] = options[prop];
 					}
 				}
-			
 			return optionsMethod;
-			
-			
 		}
 		,
 		_photos:function(method,options){
-			$.extend($.attachFlickr.settings, options);
-	
-			$.attachFlickr.methods.__clearContainer();
-			var URL = $.attachFlickr.methods.__url(method,$.attachFlickr.methods.__createObjectMethod(method));
+						
+			$.flickr.methods.__clearContainer(options);
+			
+			var URL = $.flickr.methods.__url(method,$.flickr.methods.__createObjectMethod(method,options));
 			
 			var html = '<!-- photos -->';
 			var response = $.getJSON(URL, function(data) {
@@ -200,7 +213,7 @@
 					var photo = data.photos.photo;
 					if(photo){
 						$(photo).each(function(item, value){
-							var src = $.attachFlickr.methods.__src(value, $.attachFlickr.settings.thumbnail_size);
+							var src = $.flickr.methods.__src(value, options.thumbnail_size);
 							var alt = value.title;
 							var id = value.id;
 							var title = '#'+value.id + '-description';
@@ -211,20 +224,20 @@
 							img.alt = alt;
 							img.id = id;
 													
-							$(img).attr($.attachFlickr.settings.titleImg,title);
+							$(img).attr(options.titleImg,title);
 							$(img).attr('secret',secret);
 							
 							var element = img;
 	
-							if($.attachFlickr.settings.imageLink){
-								element = $.attachFlickr.methods.__linkTag(value,src).append(img)
+							if(options.imageLink){
+								element = $.flickr.methods.__linkTag(value,src,options).append(img)
 							}
 							
 							var newElement = element
 							
-							if($.attachFlickr.settings.repeattpl){
-								newElement = $.attachFlickr.methods.__repeatTPL(element);
-								console.log('repeatTPL')
+							if(options.repeattpl){
+								newElement = $.flickr.methods.__repeatTPL(element,options);
+								//console.log('repeatTPL')
 							}
 							
 							imageslist.push(newElement);
@@ -232,36 +245,35 @@
 						});
 					}
 				}
-				$.attachFlickr.settings.photos = imageslist;
+				options.photos = imageslist;
 								
 			}).complete(function(){
-				if($.attachFlickr.settings.description){
-					$.attachFlickr.settings.images_description = new Array();
-					$.attachFlickr.methods.getInfo('flickr.photos.getInfo',$.attachFlickr.settings);
+				if(options.description){
+					options.images_description = new Array();
+					$.flickr.methods.getInfo('flickr.photos.getInfo',options);
 				}else{
-					$.attachFlickr.methods.complete_panel();
+					$.flickr.methods.complete_panel(options);
 				}
 			}).error(function() { 
-				$.attachFlickr.methods.error();
+				$.flickr.methods.error();
 			});
 			
-			var imagesList = $.attachFlickr.settings.photos;
+			var imagesList = options.photos;
 			return imagesList;
 		},
 		getInfo: function(method,options){
-			$.extend($.attachFlickr.settings, options);
 			
-			$.attachFlickr.settings.totalInfo = $.attachFlickr.settings.photos.length;
-			$.attachFlickr.settings.totalInfoCompleted = 1;
+			options.totalInfo = options.photos.length;
+			options.totalInfoCompleted = 1;
 			
-			//console.log($.attachFlickr.settings.totalInfo);
+			//console.log(options.totalInfo);
 			
-			$($.attachFlickr.settings.photos).each(function(item,value){
+			$(options.photos).each(function(item,value){
 				
 				var photoId = '0';
 				var secret = '0';
 				
-				if($.attachFlickr.settings.imageLink){
+				if(options.imageLink){
 					photoId = $(value).find('img').attr('id');
 					secret = $(value).find('img').attr('secret');
 				}else{
@@ -271,11 +283,11 @@
 				
 				//http://www.flickr.com/services/api/flickr.photos.getInfo.html
 				var optionsMethod = {};
-				optionsMethod.api_key = $.attachFlickr.settings.api_key;
+				optionsMethod.api_key = options.api_key;
 				optionsMethod.secret = secret;
 				optionsMethod.photo_id = photoId;
 				
-				var URL = $.attachFlickr.methods.__url(method, optionsMethod);
+				var URL = $.flickr.methods.__url(method, optionsMethod);
 				var html = '<!-- photos -->';
 				$.getJSON(URL, function(data) {
 					
@@ -284,13 +296,13 @@
 						var dateTaken = data.photo.dates.taken;
 						var tags = data.photo.tags.tag;
 						
-						var tpl = $('<div class="desc-content '+$.attachFlickr.settings.desCotentCSS+'" id="'+photoId+'-description"></div>');
-						tpl.append('<span class="desc '+$.attachFlickr.settings.descCSS+'">'+description+'</span>');
-						tpl.append('<span class="date '+$.attachFlickr.settings.dateCSS+'">'+dateTaken+'</span>');
+						var tpl = $('<div class="desc-content '+options.desCotentCSS+'" id="'+photoId+'-description"></div>');
+						tpl.append('<span class="desc '+options.descCSS+'">'+description+'</span>');
+						tpl.append('<span class="date '+options.dateCSS+'">'+dateTaken+'</span>');
 
 						var inTpl = '<div class="tags">';
 						$(tags).each(function(item,value){
-							inTpl += '<span class="tag '+$.attachFlickr.settings.tagCSS+'">'+value._content+'</span>';
+							inTpl += '<span class="tag '+options.tagCSS+'">'+value._content+'</span>';
 						});
 						inTpl += '</div>';
 						
@@ -298,12 +310,12 @@
 
 					}
 					
-					$.attachFlickr.settings.images_description.push(tpl);
+					options.images_description.push(tpl);
 				}).complete(function(){
-					if($.attachFlickr.settings.totalInfoCompleted == $.attachFlickr.settings.totalInfo){
-						$.attachFlickr.methods.complete_panel();
+					if(options.totalInfoCompleted == options.totalInfo){
+						$.flickr.methods.complete_panel(options);
 					}
-					$.attachFlickr.settings.totalInfoCompleted++;
+					options.totalInfoCompleted++;
 				});
 				
 			})
@@ -313,7 +325,7 @@
 			
 			//console.log('__url');
 			
-			var _url = 'http://api.flickr.com/services/rest/?method=' + method + '&format=json' + ($.attachFlickr.methods.__isEmpty(params) ? '' : '&' + $.param(params)) + '&jsoncallback=?';
+			var _url = 'http://api.flickr.com/services/rest/?method=' + method + '&format=json' + ($.flickr.methods.__isEmpty(params) ? '' : '&' + $.param(params)) + '&jsoncallback=?';
 		  
 		  	//console.log(_url);
 		  
@@ -331,7 +343,7 @@
 		},
 		__src: function(photo, size) {
 			
-		  var translate_size = (size == undefined) ? '_s' : $.attachFlickr.methods.__translate(size);
+		  var translate_size = (size == undefined) ? '_s' : $.flickr.methods.__translate(size);
 		  
 		  //console.log('translate_size : '+translate_size);
 		 
@@ -350,19 +362,19 @@
 			default : return '' // medium
 		  }
 		},
-		__linkTag: function(photo, href) {
+		__linkTag: function(photo, href,options) {
 		  if (href === undefined) href = ['http://www.flickr.com/photos', photo.owner, photo.id].join('/')
-		  return $('<a href="' + href + '" title="' + photo.title + '"></a>');
+		  return $('<a href="' + href + '" title="' + photo.title + '" class="'+options.linkTagCSS+'"></a>');
 		},
-		__repeatTPL: function(obj) {
-		  var str = $.attachFlickr.settings.repeattpl;
+		__repeatTPL: function(obj,options) {
+		  var str = options.repeattpl;
 		  str.replace(/{images}/gi, '').replace(/{description}/gi, '');
 		  
 		  return $(str).append(obj);
 		},
-		__clearContainer: function(){
-			if($.attachFlickr.settings.clearContainer){
-				$($.attachFlickr.settings._self).html('');
+		__clearContainer: function(options){
+			if(options.clearContainer){
+				$(options.settings._self).html('');
 			};
 		}
 	};
@@ -370,10 +382,10 @@
 	
   $.fn.aFlickr = function(method) {	
 	
-    if ( $.attachFlickr.methods[method] ) {
-      return $.attachFlickr.methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    if ( $.flickr.methods[method] ) {
+      return $.flickr.methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
     } else if ( typeof method === 'object' || ! method ) {
-      return $.attachFlickr.methods.init.apply( this, arguments );
+      return $.flickr.methods.init.apply( this, arguments );
     } else {
       $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
     }  
